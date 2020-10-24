cb3a589cec2cfe27395f67d8bac25ca00a1cb519
Chris Liaw <chrisliaw@antrapol.com>
2020-10-24 18:01:58 +0800

    Adding push tag

--- lib/git_cli/push.rb
+++ lib/git_cli/push.rb
@@ -5,7 +5,7 @@ module GitCli
 
     def push_changes(repos, branch = "master")
       check_vcs
-      check_repos
+      #check_repos
       raise_if_empty(repos, "Push to repository name cannot be empty", GitCliException)
 
       raise_if_false(is_repos_exist?(repos), "Given repository name '#{repos}' is not configured for this workspace", GitCliException)
@@ -25,12 +25,12 @@ module GitCli
       os_exec(cmdln) do |st, res|
         [st.success?, res.strip]
       end
-    end
+    end # push_changes
     alias :push :push_changes
 
-    def push_tags(repos, branch = "master")
+    def push_changes_with_tags(repos, branch = "master")
       check_vcs
-      check_repos
+      #check_repos
       raise_if_empty(repos, "Push to repository name cannot be empty", GitCliException)
       
       raise_if_false(is_repos_exist?(repos), "Given repository name '#{repos}' is not configured for this workspace", GitCliException)
@@ -47,13 +47,14 @@ module GitCli
 
       cmdln = cmd.join " "
 
-      log_debug "Push tags : #{cmdln}"
+      log_debug "Push with tags : #{cmdln}"
       os_exec(cmdln) do |st, res|
         [st.success?, res.strip]
       end
-    end
-
-
+    end # push_changes_with_tags
+    alias :push_with_tags :push_changes_with_tags
+    alias :push_with_tag  :push_changes_with_tags
+    alias :push_changes_with_tag :push_changes_with_tags
 
   end
 end
